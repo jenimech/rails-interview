ca4d46c546ea196d447923f57898a591b024afb5
Maria E. Chacon <maria.chacon@globant.com>
2017-03-03 14:35:24 -0300

    Added methods name, movie and self.all_by_movie to filter the students by the movie passed as param

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,20 @@
 class Student < ActiveRecord::Base
+  scope :all_except, ->(student) { where.not(id: student) }
+
+  def name
+    names = Student.all_except(self).map(&:firstname).uniq
+    names.include?(firstname) ? "#{firstname} #{lastname[0,1]}." : firstname
+  end
+
+  def movie
+    favoritemovie
+  end
+
+  def self.movies
+    self.all.map(&:favoritemovie).uniq
+  end
+
+  def self.all_by_movie(movie)
+    self.all.select {|s| s.favoritemovie.downcase == movie.downcase }
+  end
 end
